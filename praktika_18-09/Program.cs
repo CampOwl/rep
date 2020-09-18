b4391327fef68cd999a30f36f34238f2ba911d5e
campowl <nastya-zmeika@mail.ru>
2020-09-18 09:59:51 +0300

    третья практика

--- praktika_18-09/Program.cs
+++ praktika_18-09/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace praktika_18_09
+namespace ConsoleApp1
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      string[] text ={"Макс",
+        "C++",
+        " +79263772622",
+        " 89263772622",
+        " (926)3772622",
+      " +7-945-677-44-55",
+      "8(098)456-56-56"};
+
+      foreach (string item in text)
+      {
+        string pattern = @"[+7,8]{0,1}[(,-]{0,1}\d{3}[)-]{0,1}\d{3}\-?\d{2}\-?\d{2}$";
+        if (Regex.IsMatch(item, pattern))
+        {
+          //Console.WriteLine($"{item} это телефон"); 
+          Console.WriteLine(item + " это телефон");
+        }
+        else
+        {
+          Console.WriteLine(
+          string.Format("{0} это НЕ телефон", item)
+          );
+        }
+
+      }
+
     }
   }
 }
