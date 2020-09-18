5a0605cadc385b87974df6d0c6fc163de9088b38
campowl <nastya-zmeika@mail.ru>
2020-09-18 11:17:07 +0300

    очередь и стэк

---
+++ praktika-4-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace prak4_02_upr01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Queue queue = new Queue();//очередь
+
+      queue.Enqueue("1");
+      queue.Enqueue("2");
+      queue.Enqueue("3");
+      queue.Enqueue("4");
+
+      while (queue.Count > 0)
+      {
+        object obj = queue.Dequeue();
+        Console.WriteLine("From Queue: {0}", obj);
+      }
+      Stack stack = new Stack();//стэк
+      stack.Push("1");
+      stack.Push("2");
+      stack.Push("3");
+      stack.Push("4");
+      while (stack.Count > 0)
+      {
+        object obj = stack.Pop();
+        Console.WriteLine("'From Stack: {0}", obj);
+      }
+
+    }
+  }
+}
