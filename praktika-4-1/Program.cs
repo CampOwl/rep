f50adfe40b9a906bad5236f3ad4539f7f3299c64
campowl <nastya-zmeika@mail.ru>
2020-09-18 11:03:11 +0300

    4-1

---
+++ praktika-4-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace prakt4_01
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      ArrayList myList = new ArrayList();//список
+      myList.Add("ABC");
+      myList.Add("cbd");
+      myList.Add("abc");
+      myList.Add("xyz");
+      foreach (string item in myList)
+      {
+        Console.WriteLine("Unsorted: {0}", item);
+      }
+      // Сортировка при помощи стандартного объекта сравнения 
+      myList.Sort();
+      foreach (string item in myList)
+      {
+        Console.WriteLine("   Sorted: {0}", item);
+      }
+
+    }
+
+  }
+}
