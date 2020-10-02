5f687ec427430389a23e8a425bf00e2d9491f53b
camp owl <nastya-zmeika@mail.ru>
2020-10-02 09:43:05 +0300

    add pr5 01

--- praktika-5-1/CodeFile1.cs
+++ praktika-5-1/CodeFile1.cs
@@ -71,9 +71,16 @@ namespace Serialize_People
     {
       Person dsp = new Person();
 
-      // TODO: Restore previously serialized Person object
+      // Открываем файл для чтения данных	
+      FileStream fs = new FileStream("Person.Dat", FileMode.Open);
+      // Создаем объект BinaryFormatter для выполнения десериализации
+      BinaryFormatter bf = new BinaryFormatter();
+      // Используем объект BinaryFormatter для десериализации данных из файла
+      dsp = (Person)bf.Deserialize(fs);
+      // Закрываем файл fs.Close();
 
       return dsp;
     }
+
   }
 }
